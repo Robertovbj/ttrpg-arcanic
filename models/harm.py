080d4c66074a14d949fc2ca08ce9582e0b4418b2
Roberto Barbosa <roberto.rvbj@gmail.com>
2023-03-12 15:19:24 -0300

    Harm bar is now vertical

--- models/harm.py
+++ models/harm.py
@@ -6,21 +6,21 @@ class HarmPage(Page):
     def __init__(self):
         super().__init__("Harm")
         self.fields = [
-            Field("HARM", "-" * 14 + "3" + "-" * 8 + "6" + "-" * 8 + "9" + "-10-11-" + f"""{SKULL_CROSSBONES}-------
-            12am |{GREEN_SQUARE}{GREEN_SQUARE}{GREEN_SQUARE}{GREEN_SQUARE}{GREEN_SQUARE}{GREEN_SQUARE}{YELLOW_SQUARE}{YELLOW_SQUARE}{YELLOW_SQUARE}{ORANGE_SQUARE}{ORANGE_SQUARE}{RED_SQUARE}| 12pm""", True),
-            Field("Conditions", "Crippled\nBroken")
-            # Field("HARM", f"""12am
-            # {GREEN_SQUARE}
-            # {GREEN_SQUARE}
-            # {GREEN_SQUARE} 3
-            # {GREEN_SQUARE}
-            # {GREEN_SQUARE}
-            # {GREEN_SQUARE} 3
-            # {YELLOW_SQUARE}
-            # {YELLOW_SQUARE}
-            # {YELLOW_SQUARE} 9
-            # {ORANGE_SQUARE} 10
-            # {ORANGE_SQUARE} 11
-            # {RED_SQUARE} {SKULL_CROSSBONES}
-            # 12pm""", True)
+            # Field("HARM", "-" * 14 + "3" + "-" * 8 + "6" + "-" * 8 + "9" + "-10-11-" + f"""{SKULL_CROSSBONES}-------
+            # 12am |{GREEN_SQUARE}{GREEN_SQUARE}{GREEN_SQUARE}{GREEN_SQUARE}{GREEN_SQUARE}{GREEN_SQUARE}{YELLOW_SQUARE}{YELLOW_SQUARE}{YELLOW_SQUARE}{ORANGE_SQUARE}{ORANGE_SQUARE}{RED_SQUARE}| 12pm""", True),
+            Field("HARM", f"""12pm
+            {RED_SQUARE} {SKULL_CROSSBONES}
+            {ORANGE_SQUARE} 11
+            {ORANGE_SQUARE} 10
+            {YELLOW_SQUARE} 9
+            {YELLOW_SQUARE}
+            {YELLOW_SQUARE}
+            {GREEN_SQUARE} 6
+            {GREEN_SQUARE}
+            {GREEN_SQUARE}
+            {GREEN_SQUARE} 3
+            {GREEN_SQUARE}
+            {GREEN_SQUARE}
+            12am""", True),
+            Field("Conditions", "Crippled\nBroken", True)
         ]
